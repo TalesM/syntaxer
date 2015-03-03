615afba2fd69b44e0343ea9696069272e72dfdce
talesm <tales.miranda84@gmail.com>
2015-03-03 00:24:56 -0300

    Changed the meaning of "Semantics" (get it?) for #1
    Now we need only specify conventions to do semantic checking and we are
    done.

--- js/ui/export.js
+++ js/ui/export.js
@@ -1,11 +1,52 @@
-define(['lib/DomParser', 'lib/JsonGenerator', 'domReady!'], 
-function(domParser,       JsonGenerator ,      doc) {
+define(['util/utility', 'lib/DomParser', 'lib/JsonGenerator', 'domReady!'], 
+function(utility,        domParser,       JsonGenerator ,      doc) {
 	'use strict'
-	$('.export-json').click(function(event) {
-        'use strict'
-        /* Act on the event */
+	$('.jGenerateJson').click(function(event) {
         var result = domParser.parse($(output), new JsonGenerator());
-        this.href = 'data:,'+JSON.stringify(result);
-        //return false;
+        this.href = 'data:,'+encodeURIComponent(JSON.stringify(result));
+    });
+    $('.jGenerateTokenizer').click(function(event) {
+        console.log('Generating Tokenizer');
+        // alert('Hello');
+        var objectNotation = domParser.parse($('#output'), new JsonGenerator());
+        var tokens = objectNotation.terminals;
+        var view = {
+            ignored: $('#jParamWhitespace').val(),
+            regexTokens: [],
+            extraTokens: [],
+        };
+        Object.keys(tokens).map(function(token) {
+            var definition = tokens[token];
+            if(definition===true){
+                view.extraTokens.push({
+                    token: token,
+                });
+            } else {
+                view.regexTokens.push({
+                    token: token,
+                    regex: definition,
+                });
+            }
+        });
+        var output = Mustache.render($('#jTemplTokenizer').val(), view);
+        this.href = 'data:application/javascript,'+encodeURIComponent(output);
+    });
+
+    $('.jGenerateParser').click(function() {
+        console.log('Generating Parser');
+        var objectNotation = domParser.parse($('#output'), new JsonGenerator());
+        var productions = objectNotation.productions;
+        var view = {
+            productions: [],
+        };
+        Object.keys(productions).map(function(name) {
+            var definitions = productions[name];
+            view.productions.push({
+                name: name,
+                definitions: definitions,
+            });
+        });
+        var output = Mustache.render($('#jTemplParser').val(), view);
+        this.href = 'data:application/javascript,'+encodeURIComponent(output);
     });
 });
