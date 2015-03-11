042982b23f01cd879816691400663e76c385be81
talesm <tales.miranda84@gmail.com>
2015-03-10 23:12:47 -0300

    Put the semantic type as template.

--- js/ui/semantics.js
+++ js/ui/semantics.js
@@ -1,4 +1,6 @@
-define([],
-function() {
+define(['domReady!', 'text!templ/semantic.html'],
+function(dom,         templSemantic            ) {
+    $.Mustache.add('templ-semantic', templSemantic);
+	$('.semantics-cointainer').empty().mustache('templ-semantic', [{}, {}, {}, {}, {}]);
     return {};
 }); 
--- js/util/templates.js
+++ js/util/templates.js
@@ -1,5 +1,5 @@
-define(['util/utility', 'text!templ/syntax.html', 'text!templ/edit.html', 'text!templ/Tokenizer.js', 'text!templ/Parser.js', 'domReady!'],
-function(utility,        templOutput,              templEdit,              templTokenizer,            templParser,            doc) {
+define(['util/utility', 'text!templ/syntax.html', 'text!templ/edit.html', 'text!templ/semantic.html', 'text!templ/Tokenizer.js', 'text!templ/Parser.js', 'domReady!'],
+function(utility,        templOutput,              templEdit,              templSemantic,              templTokenizer,            templParser,            doc) {
     'use strict'
     $.Mustache.add('templ-output', templOutput);
     $.Mustache.add('templ-edit', templEdit);
