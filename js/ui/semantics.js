3a2a0b0ae6687c8f239216cafb1b35c8d1734dc5
talesm <tales.miranda84@gmail.com>
2015-03-10 23:41:51 -0300

    Added delete, move stack types.
    For task #8

--- js/ui/semantics.js
+++ js/ui/semantics.js
@@ -1,6 +1,45 @@
 define(['domReady!', 'text!templ/semantic.html'],
 function(dom,         templSemantic            ) {
+	'use strict';
     $.Mustache.add('templ-semantic', templSemantic);
-	$('.semantics-cointainer').empty().mustache('templ-semantic', [{}, {}, {}, {}, {}]);
+    var $semanticsContainer = $('.semantics-cointainer');
+	$semanticsContainer.empty().mustache('templ-semantic', [
+		{
+			name: 'Stack1',
+			fields: [],
+			operations: [],
+		}, 
+		{
+			name: 'Stack2',
+			fields: [],
+			operations: [],
+		}, 
+		{
+			name: 'Stack3',
+			fields: [],
+			operations: [],
+		}, 
+		{
+			name: 'Stack4',
+			fields: [],
+			operations: [],
+		}, 
+		{
+			name: 'Stack5',
+			fields: [],
+			operations: [],
+		}, 
+	]);
+	$semanticsContainer.on('click', '.jDelete', function() {
+		$(this).closest('.type').remove();
+	});
+	$semanticsContainer.on('click', '.jRight', function() {
+		var $thisType = $(this).closest('.type');
+		$thisType.next().after($thisType);
+	});
+	$semanticsContainer.on('click', '.jLeft', function() {
+		var $thisType = $(this).closest('.type');
+		$thisType.prev().before($thisType);
+	});
     return {};
 }); 
