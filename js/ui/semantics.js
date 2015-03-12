8d0ec0dbf3d592689f95b8227dd9ca39ba5a5545
talesm <tales.miranda84@gmail.com>
2015-03-12 19:44:59 -0300

    Erased semantics... By now.

--- js/ui/semantics.js
+++ js/ui/semantics.js
@@ -1,13 +1,22 @@
 define(['domReady!', 'text!templ/semantics.html'],
 function(dom,         templSemantics            ) { 
 	'use strict';
+	return;
     $.Mustache.add('templ-semantics', templSemantics);
     var $semanticsContainer = $('.semantics-cointainer');
 	$semanticsContainer.empty().mustache('templ-semantics', [
 		{
 			name: 'Stack1',
-			fields: [],
-			operations: [],
+			fields: [
+				{name: 'name1', value: 'value1'},
+				{name: 'name2', value: 'value2'},
+				{name: 'name3', value: 'value3'},
+			],
+			operations: [
+				{name: 'name1', stms: 'value1'},
+				{name: 'name2', value: 'value2'},
+				{name: 'name3', value: 'value3'},
+			],
 		}, 
 		{
 			name: 'Stack2',
