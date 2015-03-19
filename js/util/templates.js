a654015963b7c3bdaa51249fdbbd513c672377a3
talesm <tales.miranda84@gmail.com>
2015-03-19 11:09:12 -0300

    Fixed a case typo that prevented it working on linux systems

--- js/util/templates.js
+++ js/util/templates.js
@@ -1,4 +1,4 @@
-define(['util/utility', 'text!templ/Definition.html', 'text!templ/syntax.html', 'text!templ/edit.html', 'text!templ/Tokenizer.js', 'text!templ/Parser.js', 'domReady!'],
+define(['util/utility', 'text!templ/definition.html', 'text!templ/syntax.html', 'text!templ/edit.html', 'text!templ/Tokenizer.js', 'text!templ/Parser.js', 'domReady!'],
 function(utility,        templDefinition,              templOutput,              templEdit,              templTokenizer,            templParser,            doc) {
     'use strict'
     $.Mustache.add('definition', templDefinition);
